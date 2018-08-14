ad690c773eff68f4d100e04eec8330948f1d8024
JohnnyAde <42357974+JohnnyAde@users.noreply.github.com>
2018-08-14 20:43:17 +0100

    Assignment 1 and 2

---
+++ statistics.py
@@ -0,0 +1,13 @@
+#assignment2
+age=float(input('inputtheage\n'))
+name=float(input('inputthename\n'))
+age=100
+print(age)
+
+#assignment1
+b=float(input('inputtheb\n'))
+a=float(input('inputthea\n'))
+c=float(input('inputthec\n'))
+
+x=(-b+4*a*c)/2*a
+print(x)
